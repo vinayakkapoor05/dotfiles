1b45349aa25b3dae73eb26135aa3dece579fed06
Vinayak Kapoor <72437558+vinayakkapoor05@users.noreply.github.com>
2026-04-14 16:40:35 -0500

    10x dev larp

---
+++ nvim/lua/plugins/lazygit.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "kdheepak/lazygit.nvim",
+    cmd = "LazyGit",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    keys = {
+      { "<leader>gg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+    },
+  },
+}
